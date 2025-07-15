7d5ee41872574524dd0c7d17956ab29c3edfe912
Deen Bandhu <141038043+Deenbandhu01@users.noreply.github.com>
2025-07-15 17:32:27 +0000

    variable

---
+++ Basics/day_01.js
@@ -0,0 +1,19 @@
+// let , const , var
+
+const accountId = 1444553
+let accountEmail = "deen@google.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+/*
+accountId =456
+this will be not allowed
+*/
+
+console.log("DEEN")
