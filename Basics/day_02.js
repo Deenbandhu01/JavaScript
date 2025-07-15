c726f45033e0813d89b0b096e99782706eb52bca
Deen Bandhu <141038043+Deenbandhu01@users.noreply.github.com>
2025-07-15 17:34:23 +0000

    datatypes

---
+++ Basics/day_02.js
@@ -0,0 +1,26 @@
+//datatypes
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // it will not work because we are using nodejs, not browser
+
+let name = "deen"
+let age = 18
+let isLoggedIn = false
+let temp = null
+let state
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
