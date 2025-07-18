e65698c28099865255dd4533d6d350705625f5a7
Deen Bandhu <141038043+Deenbandhu01@users.noreply.github.com>
2025-07-18 18:14:41 +0000

    Adding some more topics

---
+++ Basics/03_conversion.js
@@ -0,0 +1,38 @@
+let score = "33a";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 = true ; 0 = false
+//"" = false
+// "deen" = true
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
+
+let value = 3 
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " deen"
+let str3 = str1 + str2
+console.log(str3);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+let gameConter =100;
+gameConter++
+console.log(gameConter);
+
---
+++ Basics/05_StackAndHeap.js
@@ -0,0 +1,22 @@
+// Stack (Primitive), Heap(Non-Primitive)
+let myYoutubeName = "bandhu cartoon world"
+
+let anotherName = myYoutubeName
+
+anotherName = "bandhu engineering world"
+
+console.log(anotherName);
+
+
+let user ={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+console.log(user)
+
+let userTwo = user
+userTwo.email = "deenbandhu@google.com"
+
+console.log(userTwo)
+console.log(user)
